049974ce020e073469b77e3963e0c1fae9a03490
zdc524 <zdc524@sohu.com>
2015-06-11 16:15:14 +0800

    fsdf

--- src/net/oschina/jedis/JedisUtils.java
+++ src/net/oschina/jedis/JedisUtils.java
@@ -73,7 +73,7 @@ public class JedisUtils {
 
     /**
      * 当getInstance方法第一次被调用的时候，它第一次读取 RedisUtilHolder.instance，导致RedisUtilHolder类得到初始化；而这个类在装载并被初始化的时候，会初始化它的静
-     * 态域，从而创建RedisUtil的实例，由于是静态的域，因此只会在虚拟机装载类的时候初始化一次，并由虚拟机来保证它的线程安全性。
+     * 态域，从而创建JedisUtil的实例，由于是静态的域，因此只会在虚拟机装载类的时候初始化一次，并由虚拟机来保证它的线程安全性。
      * 这个模式的优势在于，getInstance方法并没有被同步，并且只是执行一个域的访问，因此延迟初始化并没有增加任何访问成本。
      */
     public static JedisUtils getInstance() {
