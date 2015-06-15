ead63d6d5c80a2488c3e684674f048c689cff5cf
zdc524 <zdc524@sohu.com>
2015-06-15 14:16:17 +0800

    添加redis链接参数

--- src/net/oschina/jedis/JedisUtils.java
+++ src/net/oschina/jedis/JedisUtils.java
@@ -44,13 +44,17 @@ public class JedisUtils {
 
             try {
                 /**
-                 * 如果你遇到 java.net.SocketTimeoutException: Read timed out
-				 exception的异常信息 请尝试在构造JedisPool的时候设置自己的超时值.
+                 * 如果你遇到 java.net.SocketTimeoutException: Read timed out exception的异常信息
+                 *  请尝试在构造JedisPool的时候设置自己的超时值.
                  * JedisPool默认的超时时间是2秒(单位毫秒) JedisPoolConfig
                  */
-                JedisPoolConfig name = new JedisPoolConfig();
-                // GenericObjectPoolConfig name = new GenericObjectPoolConfig();
-                pool = new JedisPool(name, ip, port, 2000);
+                JedisPoolConfig poolConfig = new JedisPoolConfig();
+                poolConfig.setMaxTotal(1024);
+                poolConfig.setMaxIdle(200);
+                poolConfig.setMaxWaitMillis(1000);
+                poolConfig.setTestOnBorrow(true);
+                
+                pool = new JedisPool(poolConfig, ip, port, 2000);
                 maps.put(key, pool);
             } catch (Exception e) {
                 log.error(e.getMessage());
@@ -62,8 +66,7 @@ public class JedisUtils {
     }
 
     /**
-     * 类级的内部类，也就是静态的成员式内部类，该内部类的实例与外部类的实例
-	 没有绑定关系，而且只有被调用到时才会装载，从而实现了延迟加载。
+     * 类级的内部类，也就是静态的成员式内部类，该内部类的实例与外部类的实例 没有绑定关系，而且只有被调用到时才会装载，从而实现了延迟加载。
      */
     private static class RedisUtilHolder {
 
@@ -74,10 +77,10 @@ public class JedisUtils {
     }
 
     /**
-     * 当getInstance方法第一次被调用的时候，它第一次读取 RedisUtilHolder.instance，导致RedisUtilHolder类得到初始化；
-	 而这个类在装载并被初始化的时候，会初始化它的静
-     * 态域，
-	 从而创建JedisUtil的实例，由于是静态的域，因此只会在虚拟机装载类的时候初始化一次，并由虚拟机来保证它的线程安全性。
+     * 当getInstance方法第一次被调用的时候，它第一次读取 RedisUtilHolder.instance，
+     * 导致RedisUtilHolder类得到初始化；而这个类在装载并被初始化的时候，会初始化它的静
+     * 态域，从而创建JedisUtil的实例，
+     * 由于是静态的域，因此只会在虚拟机装载类的时候初始化一次，并由虚拟机来保证它的线程安全性。
      * 这个模式的优势在于，getInstance方法并没有被同步，并且只是执行一个域的访问，因此延迟初始化并没有增加任何访问成本。
      */
     public static JedisUtils getInstance() {
